1517eda39e52de31708ac591849bb0dd666e2811
yirenong1995 <yirenong@cavill.biz>
2026-01-09 13:17:11 +0800

    first commit
    website for zh demo

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from './views/Dashboard.vue'
-import Analytics from './views/Analytics.vue'
-import Threshold from './views/Threshold.vue'
+// import Analytics from './views/Analytics.vue'
+// import Threshold from './views/Threshold.vue'
+import Login from './views/Login.vue'
 
 const routes = [
     { path: '/', name: 'Dashboard', component: Dashboard },
-    { path: '/analytics', name: 'Analytics', component: Analytics },
-    { path: '/threshold', name: 'Threshold', component: Threshold }
+    { path: '/login', name: 'Login', component: Login },
+    // { path: '/analytics', name: 'Analytics', component: Analytics },
+    // { path: '/threshold', name: 'Threshold', component: Threshold }
 ]
 
 export default createRouter({
